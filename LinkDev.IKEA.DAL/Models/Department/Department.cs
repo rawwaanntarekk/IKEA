e384488f674b9ce04f260e952418f688f4c92402
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-09-20 12:43:28 +0300

    Part 05 Department Module (Entities, Configrations) and DbContext.

---
+++ LinkDev.IKEA.DAL/Models/Department/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Models.Department
+{
+    // This is the data the will be in the database table.
+    internal class Department : ModelBase
+    {
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        // The date the deprtment created in the company.
+        public DateOnly CreationDate { get; set; }
+
+    }
+}
---
+++ LinkDev.IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using LinkDev.IKEA.DAL.Models.Department;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =DESKTOP-QK0IFST\\MSSQLSERVERR; Database = IKEA; Trusted_Connection = True; TrustServerCertificate = Yes; MultipleActiveResultSets = False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+
+        DbSet<Department> Departments { get; set; } = null!;
+    }
+}
