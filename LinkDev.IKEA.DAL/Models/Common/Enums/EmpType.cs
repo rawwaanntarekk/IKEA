b0284904491d978f972d97df97384ed7f48f69ea
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-09-21 15:31:56 +0300

    Part 03 Employee Module - Entities, Configs, Migration.

---
+++ LinkDev.IKEA.DAL/Models/Common/Enums/EmpType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Models.Common.Enums
+{
+    public enum EmpType
+    {
+        FullTime,
+        PartTime
+    }
+}
---
+++ LinkDev.IKEA.DAL/Persistance/Data/Configurations/Employees/EmployeeConfigurations.cs
@@ -0,0 +1,36 @@
+﻿using LinkDev.IKEA.DAL.Models.Common.Enums;
+using LinkDev.IKEA.DAL.Models.Employees;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistance.Data.Configurations.Employees
+{
+    internal class EmployeeConfigurations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+          builder.Property(E => E.Name).HasColumnType("nvarchar(50)");
+          builder.Property(E => E.Address).HasColumnType("nvarchar(100)");
+          builder.Property(E => E.Salary).HasColumnType("decimal(8,2)");
+          builder.Property(E => E.CreatedOn).HasDefaultValue(DateTime.UtcNow);
+          builder.Property(E => E.Gender)
+                 .HasConversion((gender) => gender.ToString(),
+                 (gender) => (Gender) Enum.Parse(typeof(Gender), gender));
+
+          builder.Property(E => E.EmployeeType)
+                 .HasConversion(
+                 (empType) => empType.ToString(),
+                 (empType) => (EmpType)Enum.Parse(typeof(EmpType), empType));
+
+
+
+
+
+        }
+    }
+}
